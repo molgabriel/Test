ba503f2a5aa81ab7ab0bc01a2cd031850b729bb3
Molnár Gabriella <molgabriel@hotmail.com>
2017-03-07 21:12:38 +0100

    sata11
    a vége nem önálló munka

--- Test/main.c
+++ Test/main.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Töltsön fel egy egész típusú 100db elemet tartalmazó tömböt páros számokkal 100-tól növekvő értékkel!
+Keresse meg a második legnagyobb értékű elemet a tömbben! Keresse meg a legkisebb elemet a tömbben!*/
 int main()
 {
-    int i;
+    int a=0,o=-31313 ,max=-212212, tmb[100], k=100, min=21010101;
 
-    printf("Hello world!\n");
+    for(a=0;a<100;a++)
+    {
+        if(k%2==0)
+        {
+            tmb[a]=k;
+        }
+        else
+        {
+            ++k;
+            tmb[a]=k;
+        }
+        k++;
+    }
+
+
+    for(a=0;a<100;a++)
+    {
+
+        o=o<tmb[a]?tmb[a]:o;
+
+
+        min=min<tmb[a]?min:tmb[a];
+
+    }
+    for(a=0;a<100;a++)
+    {
+        max=(max>tmb[a]||tmb[a]==o)&&max<o?max:tmb[a];
+    }
+
+
+    return 0;
 }
